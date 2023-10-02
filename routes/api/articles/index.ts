8d805018b09afb9054d685a2ace8dd463828d668
Guigui0025 <144000296+Guigui0025@users.noreply.github.com>
2023-10-02 10:10:52 +0200

    2 files modified, 4 added, 4 deleted

---
+++ routes/api/articles/index.ts
@@ -0,0 +1,11 @@
+
+
+import { useStorage } from '#imports';
+
+export default defineEventHandler(async () => {
+  const articles: articles = await useStorage('assets:server').getItem(
+    `db/articles.json`
+  );
+
+  return articles;
+});
